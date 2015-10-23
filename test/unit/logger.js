306dced9b1b8cc59f52d7b656b02034526f02db7
Casey Flynn <cflynn.us@gmail.com>
2015-10-22 17:31:48 -0700

    Refactor logger unit tests

--- test/unit/logger.js
+++ test/unit/logger.js
@@ -1,18 +1,68 @@
+/**
+ * @module test/unit/logger
+ */
 'use strict';
 
 var Lab = require('lab');
+var Code = require('code');
+var domain  = require('domain');
+
 var lab = exports.lab = Lab.script();
+
 var describe = lab.describe;
+var expect = Code.expect;
 var it = lab.test;
 
 var logger = require('../../lib/logger');
 
 describe('lib/logger.js unit test', function () {
-  it('should cover serializers', function (done) {
-    logger.fatal({
-      tx: true,
-      req: {}
-    }, 'msg');
-    done();
+  describe('serializers', function () {
+    describe('tx', function () {
+      it('should use data from domain', function (done) {
+        var d = domain.create();
+        d.runnableData = {
+          foo: 'bar'
+        };
+        d.run(function () {
+          var serialized = logger._serializers.tx();
+          expect(serialized.txTimestamp).to.be.an.instanceOf(Date);
+          expect(serialized.foo).to.equal('bar');
+          done();
+        });
+      });
+
+      it('should use existing domain.reqStart', function (done) {
+        var d = domain.create();
+        d.runnableData = {
+          reqStart: new Date()
+        };
+        d.run(function () {
+          var serialized = logger._serializers.tx();
+          expect(serialized.txTimestamp).to.be.an.instanceOf(Date);
+          expect(serialized.txMSFromReqStart).to.be.a.number();
+          done();
+        });
+      });
+
+      it('should work when domain.runnableData not defined', function (done) {
+        var serialized = logger._serializers.tx();
+        expect(serialized.txTimestamp).to.be.an.instanceOf(Date);
+        done();
+      });
+    });
+
+    describe('req', function () {
+      it('should parse keys from req object', function (done) {
+        var serialized = logger._serializers.req({
+          method: 'GET',
+          url: 'some-url',
+          isInternalRequest: true
+        });
+        expect(serialized.method).to.equal('GET');
+        expect(serialized.url).to.equal('some-url');
+        expect(serialized.isInternalRequest).to.equal(true);
+        done();
+      });
+    });
   });
 });
