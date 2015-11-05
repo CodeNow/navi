5d931472153ddfae093a8a0a541c6c4c0dabe5b3
Casey Flynn <cflynn.us@gmail.com>
2015-11-05 13:41:07 -0800

    Seed naviEntry fixture doc for tests

---
+++ test/fixture/navi-entries.js
@@ -0,0 +1,55 @@
+/**
+ * Same NaviEntry mongodb documents for testing
+ * @module test/fixture/navi-entries
+ */
+'use strict';
+
+module.exports = {
+  elasticUrl: 'api-staging-codenow.runnableapp.com',
+  directUrls: {
+    'e4rov2': {
+      branch: 'master',
+      dockerHost: '0.0.0.0',
+      ports: {
+        '80': 39940,
+        '8080': 23453
+      },
+      status: 'running',
+      associations: {}
+    },
+    'f8k3v2': {
+      branch: 'feature-branch1',
+      dockerHost: '0.0.0.0',
+      ports: {
+        '80': 39940,
+        '8080': 23453
+      },
+      status: 'stopped',
+      associations: {}
+    },
+    'e4v7ve': {
+      branch: 'feature-branch2',
+      dockerHost: '0.0.0.0',
+      ports: {
+        '80': 39940,
+        '8080': 23453
+      },
+      status: 'running',
+      associations: {}
+    },
+    'fukw3w': {
+      branch: 'feature-branch3',
+      dockerHost: '0.0.0.0',
+      ports: {
+        '80': 39940,
+        '8080': 23453
+      },
+      status: 'stopped',
+      associations: {}
+    },
+  },
+  userMappings: {
+    '847390': 'f8k3v2'
+  },
+  ownerGithubId: 958313
+};
