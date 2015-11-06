947664e9da1a72dcaa32d76271a9e4b76a4c7907
Bryan Kendall <bryan@runnable.com>
2015-11-05 16:36:20 -0800

    add cluster-man for clustering navi

--- index.js
+++ index.js
@@ -4,23 +4,43 @@
 'use strict';
 require('loadenv.js');
 
+var App = require('./lib/app.js');
+var ClusterManager = require('cluster-man');
+var log = require('middlewares/logger')(__filename).log;
+var numCPUs = require('os').cpus().length;
+
 if (process.env.NEWRELIC_KEY) {
   require('newrelic');
 }
 
-var App = require('./lib/app.js');
-var log = require('middlewares/logger')(__filename).log;
-
-var app = new App();
-
-app.start(function (err) {
-  if (err) {
-    log.error({
-      err: err
-    }, 'app.start error');
-    process.exit(1);
-  }
-  else {
-    log.info('app.start success');
+var manager = new ClusterManager({
+  worker: function () {
+    var app = new App();
+    app.start(function (err) {
+      if (err) {
+        log.error({
+          err: err
+        }, 'app.start error');
+        process.exit(1);
+      }
+      else {
+        log.info('app.start success');
+      }
+    });
+  },
+  master: function () {
+    log.info('app.start master');
+  },
+  numWorkers: process.env.CLUSTER_WORKERS || numCPUs,
+  killOnError: false,
+  beforeExit: function (err, done) {
+    if (err) {
+      log.error({ err: err }, 'manager.beforeExit error');
+    } else {
+      log.info('manager.beforeExit');
+    }
+    done();
   }
 });
+
+manager.start();
