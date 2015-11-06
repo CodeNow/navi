f9e824c6e947fecba3e5aebb8cb420d1b7a45e5e
Bryan Kendall <bryan.a.kendall@gmail.com>
2015-11-06 15:55:56 -0800

    move require newrelic

--- index.js
+++ index.js
@@ -4,14 +4,15 @@
 'use strict';
 require('loadenv.js');
 
+// leave this newrelic require up here!
+if (process.env.NEWRELIC_KEY) {
+  require('newrelic');
+}
+
 var App = require('./lib/app.js');
 var ClusterManager = require('cluster-man');
 var log = require('middlewares/logger')(__filename).log;
 var numCPUs = require('os').cpus().length;
-
-if (process.env.NEWRELIC_KEY) {
-  require('newrelic');
-}
 
 var manager = new ClusterManager({
   worker: function () {
