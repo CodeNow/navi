bf3257eee7a1de824fb734abedde3dda2b96ac53
Casey Flynn <cflynn.us@gmail.com>
2015-11-17 12:54:43 -0800

    Change navi to proxy to detention if not signed in

--- test/functional/api.js
+++ test/functional/api.js
@@ -95,7 +95,7 @@ describe('functional test: proxy to instance container', function () {
 
   describe('browser', function () {
     describe('unathenticated', function () {
-      it('should redirect to API login', function (done) {
+      it('should proxy to detention', function (done) {
         var host = 'api-staging-codenow.runnableapp.com';
         request({
           followRedirect: false,
@@ -105,13 +105,16 @@ describe('functional test: proxy to instance container', function () {
           },
           url: 'http://localhost:'+process.env.HTTP_PORT
         }, function (err, res) {
-          expect(res.statusCode).to.equal(307);
-          expect(res.headers.location).to.contain(process.env.API_HOST);
+          expect(res.body).to.equal(
+            'ididerror;api-staging-codenow.runnableapp.com/?type=signin&'+
+            'redirectUrl=http%3A%2F%2Fapi-staging-codenow.runnableapp.com%3A80');
+          expect(res.statusCode).to.equal(200);
+          expect(res.headers.location).to.be.undefined();
           done();
         });
       });
 
-      it('should redirect to api if shared token/key does not exist in redis', function (done) {
+      it('should proxy to detention if shared token/key does not exist in redis', function (done) {
         request({
           followRedirect: false,
           headers: {
@@ -123,14 +126,17 @@ describe('functional test: proxy to instance container', function () {
           url: 'http://localhost:'+process.env.HTTP_PORT
         }, function (err, res) {
           if (err) { return done(err); }
-          expect(res.statusCode).to.equal(307);
-          expect(res.headers.location).to.contain(process.env.API_HOST);
+          expect(res.body).to.equal(
+            'ididerror;localhost:51234/?runnableappAccessToken='+
+            'doesnotexist?type=signin&redirectUrl=http%3A%2F%2Flocalhost%3A51234');
+          expect(res.statusCode).to.equal(200);
+          expect(res.headers.location).to.be.undefined();
           done();
         });
       });
 
-      it('should redirect to api if token\'s apiSessionRedisKey redis value does not contain an ' +
-         'authenticated session', function(done) {
+      it('should proxy to detention if token\'s apiSessionRedisKey redis value does not contain '+
+         'an authenticated session', function(done) {
         redis.rpush('validAccessToken', JSON.stringify({}), function (err) {
           if (err) { return done(err); }
           request({
@@ -147,8 +153,8 @@ describe('functional test: proxy to instance container', function () {
             redis.lpop('validAccessToken', function (err, result) {
               if (err) { return done(err); }
               expect(result).to.be.null(); // Validate navi popped value out of redis
-              expect(res.statusCode).to.equal(307);
-              expect(res.headers.location).to.contain(process.env.API_HOST);
+              expect(res.statusCode).to.equal(200);
+              //expect(res.headers.location).to.be.undefined();
               done();
             });
           });
@@ -182,9 +188,6 @@ describe('functional test: proxy to instance container', function () {
             expect(testTest).to.equal(testErrorText);
             var query = querystring.parse(targetInfo.query);
             expect(query.type).to.equal('signin');
-            var testUrl = url.parse(query.redirectUrl);
-            var query2 = querystring.parse(testUrl.query);
-            expect(query2.forceLogin).to.exist();
             done();
           });
         });
@@ -312,176 +315,3 @@ describe('functional test: proxy to instance container', function () {
     });
   });
 });
-
-
-/*
-describe('proxy to backend server', function () {
-  var testIp = '0.0.0.0';
-  var testText = '1346tweyasdf3';
-
-  var testErrorText = 'ididerror';
-  var testPort = 55555;
-  var testErrorPort = 55551;
-  var testServer;
-  var testErrorServer;
-  var app;
-
-
-
-  before(function (done) {
-   testServer = TestServer.create(testPort, testIp, testText, done);
-  });
-  before(function (done) {
-   testErrorServer = TestServer.create(testErrorPort, testIp, testErrorText, done);
-  });
-  before(function (done) {
-    redis.flushall(done);
-  });
-  beforeEach(function (done) {
-    redis.removeAllListeners();
-    app = new App();
-    app.start(done);
-  });
-  afterEach(function (done) {
-    app.stop(done);
-  });
-  after(function (done) {
-    testServer.close(done);
-  });
-  after(function (done) {
-    testErrorServer.close(done);
-  });
-  describe('not logged in', function () {
-    it('should redirect to api for auth', function (done) {
-      request({
-        followRedirect: false,
-        headers: {
-          'user-agent' : chromeUserAgent
-        },
-        url: 'http://localhost:'+process.env.HTTP_PORT
-      }, function (err, res) {
-        expect(res.statusCode).to.equal(307);
-        expect(res.headers.location).to.contain(process.env.API_HOST);
-        done();
-      });
-    });
-    it('should redirect to api if shared token/key does not exist in redis', function (done) {
-      request({
-        followRedirect: false,
-        headers: {
-          'user-agent' : chromeUserAgent
-        },
-        qs: {
-          runnableappAccessToken: 'doesnotexist'
-        },
-        url: 'http://localhost:'+process.env.HTTP_PORT
-      }, function (err, res) {
-        if (err) { return done(err); }
-        expect(res.statusCode).to.equal(307);
-        expect(res.headers.location).to.contain(process.env.API_HOST);
-        done();
-      });
-    });
-    describe('token exists', function () {
-      beforeEach(function (done) {
-        sinon.stub(redis, 'lpop', function (token, cb) {
-          expect(token).to.equal('validAccessToken');
-          cb(null, JSON.stringify({
-            cookie: 'cookie',
-            apiSessionRedisKey: 'apiSessionRedisKey'
-          }));
-        });
-        sinon.stub(redis, 'get', function (key, cb) {
-          expect(key).to.equal('apiSessionRedisKey');
-          cb(null, JSON.stringify({})); // no 'user' key === unauthenticated
-        });
-        done();
-      });
-      afterEach(function(done) {
-        redis.lpop.restore();
-        redis.get.restore();
-        done();
-      });
-      it('should redirect to api if token\'s apiSessionRedisKey redis value does not contain an ' +
-         'authenticated session', function(done) {
-        request({
-          followRedirect: false,
-          headers: {
-            'user-agent' : chromeUserAgent
-          },
-          qs: {
-            runnableappAccessToken: 'validAccessToken'
-          },
-          url: 'http://localhost:'+process.env.HTTP_PORT
-        }, function (err, res) {
-          if (err) { return done(err); }
-          expect(redis.lpop.callCount).to.equal(1);
-          expect(redis.get.callCount).to.equal(1);
-          expect(res.statusCode).to.equal(307);
-          expect(res.headers.location).to.contain(process.env.API_HOST);
-          done();
-        });
-      });
-    });
-    describe('with auth attempted before', function() {
-      var j = request.jar();
-      beforeEach(function(done) {
-        request({
-          jar: j,
-          headers: {
-            'user-agent' : chromeUserAgent
-          },
-          url: 'http://localhost:'+process.env.HTTP_PORT
-        }, done);
-      });
-      it('should proxy to error login page with force if second time', function (done) {
-        request({
-          jar: j,
-          headers: {
-            'user-agent' : chromeUserAgent
-          },
-          url: 'http://localhost:'+process.env.HTTP_PORT
-        }, function (err, res, body) {
-          if (err) { return done(err); }
-          expect(res.statusCode).to.equal(200);
-          var testTest = body.split(';')[0];
-          var targetInfo = url.parse(body.split(';')[1]);
-          expect(testTest).to.equal(testErrorText);
-          var query = querystring.parse(targetInfo.query);
-          expect(query.type).to.equal('signin');
-          var testUrl = url.parse(query.redirectUrl);
-          var query2 = querystring.parse(testUrl.query);
-          expect(query2.forceLogin).to.exist();
-          done();
-        });
-      });
-    });
-  });
-
-  describe('error in system', function () {
-    var j = request.jar();
-    beforeEach(function (done) {
-      sinon.stub(redis, 'get', function (token, cb) {
-        cb(new Error('redis-error'));
-      });
-      done();
-    });
-    afterEach(function (done) {
-      redis.get.restore();
-      done();
-    });
-    it('should return', function (done) {
-      request({
-        jar: j,
-        headers: {
-          'user-agent' : chromeUserAgent
-        },
-        url: 'http://localhost:'+process.env.HTTP_PORT
-      }, function (err, res) {
-        expect(res.statusCode).to.equal(500);
-        done();
-      });
-    });
-  });
-});
-*/
