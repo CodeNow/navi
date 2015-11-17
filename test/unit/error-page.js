71e1fa75dfcb4fa584b1ade84a6903e07a028b77
Casey Flynn <cflynn.us@gmail.com>
2015-11-17 10:33:18 -0800

    Add additional unit tests for error module

--- test/unit/error-page.js
+++ test/unit/error-page.js
@@ -9,7 +9,6 @@ var errorPage = require('models/error-page.js');
 
 var lab = exports.lab = Lab.script();
 
-var beforeEach = lab.beforeEach;
 var describe = lab.describe;
 var expect = Code.expect;
 var it = lab.it;
@@ -36,6 +35,43 @@ describe('error-page.js unit test', function () {
           'http://localhost:55551?'+
           'type=not_running&elasticUrl=api-staging-codenow.runnableapp.com'+
           '&targetBranch=master');
+      done();
+    });
+
+    it('should generate error url for unresponsive error', function (done) {
+      var proxyUrl = errorPage.generateErrorUrl('unresponsive', {
+        elasticUrl: 'api-staging-codenow.runnableapp.com',
+        targetBranch: 'master'
+      });
+      expect(proxyUrl).to
+        .equal(
+          'http://localhost:55551?'+
+          'type=unresponsive&elasticUrl=api-staging-codenow.runnableapp.com'+
+          '&targetBranch=master');
+      done();
+    });
+
+    it('should generate error url for ports error', function (done) {
+      var proxyUrl = errorPage.generateErrorUrl('ports', {
+        elasticUrl: 'api-staging-codenow.runnableapp.com',
+        targetBranch: 'master'
+      });
+      expect(proxyUrl).to
+        .equal(
+          'http://localhost:55551?'+
+          'type=ports&elasticUrl=api-staging-codenow.runnableapp.com'+
+          '&targetBranch=master');
+      done();
+    });
+
+    it('should throw if invalid error', function (done) {
+      function throws () {
+        errorPage.generateErrorUrl('skjfasghasdg', {
+          elasticUrl: 'api-staging-codenow.runnableapp.com',
+          targetBranch: 'master'
+        });
+      }
+      expect(throws).to.throw();
       done();
     });
   });
