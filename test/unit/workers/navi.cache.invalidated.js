461da2372feb8ff051b68e5757a2e1dbfb8b5215
Casey Flynn <cflynn.us@gmail.com>
2015-12-09 23:06:56 -0800

    Add unit tests for cache and worker modules

---
+++ test/unit/workers/navi.cache.invalidated.js
@@ -0,0 +1,63 @@
+/**
+ * @module test/unit/workers/navi.cache.invalidated
+ */
+'use strict';
+
+require('loadenv.js');
+
+var Lab = require('lab');
+var expect = require('code').expect;
+var keypather = require('keypather')();
+var mongodb = require('mongodb');
+var ponos = require('ponos');
+var put = require('101/put');
+var sinon = require('sinon');
+
+var cache = require('cache');
+var workerNaviCacheInvalidated = require('workers/navi.cache.invalidated');
+
+var lab = exports.lab = Lab.script();
+
+var afterEach = lab.afterEach;
+var before = lab.before;
+var beforeEach = lab.beforeEach;
+var describe = lab.describe;
+var it = lab.test;
+
+var TaskFatalError = ponos.TaskFatalError;
+
+describe('lib/workers/navi.cache.invalidated', function () {
+  beforeEach(function (done) {
+    sinon.stub(cache, 'del');
+    done();
+  });
+
+  afterEach(function (done) {
+    cache.del.restore();
+    done();
+  });
+
+  it('should throw if missing required data', function (done) {
+    workerNaviCacheInvalidated({})
+      .then(function () {
+        throw new Error('should have thrown');
+      })
+      .catch(function (err) {
+        sinon.assert.notCalled(cache.del);
+        expect(err).to.be.instanceOf(TaskFatalError);
+        done();
+      });
+  });
+
+  it('should dispose cached navi-entry document', function (done) {
+    workerNaviCacheInvalidated({
+      elasticUrl: 'elastic-url-staging.runnableapp.com'
+    })
+      .then(function () {
+        sinon.assert.calledOnce(cache.del);
+        sinon.assert.calledWith(cache.del, 'elastic-url-staging.runnableapp.com');
+        done();
+      })
+      .catch(done);
+  });
+});
